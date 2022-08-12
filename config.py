d13865ba62b11d4a6ec02836b502290a81e983de
jeanpetitt <82118752+jeanpetitt@users.noreply.github.com>
2022-08-12 08:15:07 +0100

    modification de la configuration a la connexion de base de donnee en production

--- config.py
+++ config.py
@@ -11,14 +11,16 @@ DEBUG = True
 uri = os.getenv("DATABASE_URL")  # or other relevant config var
 if uri and uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
-...
-# Database initialization
-if os.environ.get('DATABASE_URL') is None:
     basedir = os.path.abspath(os.path.dirname(__file__))
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:2002@localhost/fyyur'
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False
-else:
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+...
+# Database initialization
+# if os.environ.get('DATABASE_URL') is None:
+#     basedir = os.path.abspath(os.path.dirname(__file__))
+#     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:2002@localhost/fyyur'
+#     # SQLALCHEMY_TRACK_MODIFICATIONS = False
+# else:
+#     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 # TODO IMPLEMENT DATABASE URL
 # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:2002@localhost/fyyur'
